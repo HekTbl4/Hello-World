53a4141a5bdaff4a547e1f8903d1df0397b6bedb
HekTbl4 <bananza21@gmail.com>
2019-07-17 23:23:29 +0300

    Idea commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * My First program.
+ *
+ * @author Evgeniy Barabanov
+ * @version dated 17.07.2019
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!"); //output
+    }
+}
